10ff07116a59dfbc1541cc2be40bb149b843f680
RachSavoy <rachael.a.hakim@gmail.com>
2021-06-23 14:52:03 +0900

    ARCH: authentication added to application controller.

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
   # get 'profile/edit'
   # get 'profile/update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  devise_for :users
   root to: 'pages#home'
   get '/dashboard', to: 'pages#dashboard'
 end
