cc4c6ab781a770575878594f191bfc9acd5b118e
RachSavoy <rachael.a.hakim@gmail.com>
2021-06-25 15:52:09 +0900

    ARCH: fixing routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,36 @@
 Rails.application.routes.draw do
-  # get 'checklist/show'
-  # get 'checklist/edit'
-  # get 'checklist/index'
-  # get 'checklist/update'
-  # get 'checklist/destroy'
-  # get 'messages/create'
-  # get 'chatroom/show'
-  # get 'onboarding/index'
-  # get 'onboarding/show'
-  # get 'onboarding/edit'
-  # get 'onboarding/destroy'
-  # get 'profile/index'
-  # get 'profile/show'
-  # get 'profile/view'
-  # get 'profile/create'
-  # get 'profile/destroy'
-  # get 'profile/edit'
-  # get 'profile/update'
+  get 'checklist/show'
+  get 'checklist/edit'
+  get 'checklist/index'
+  get 'checklist/update'
+  get 'checklist/destroy'
+  get 'messages/create'
+  get 'chatroom/show'
+  get 'onboarding/index'
+  get 'onboarding/show'
+  get 'onboarding/edit'
+  get 'onboarding/destroy'
+  get 'profile/index'
+  get 'profile/show'
+  get 'profile/view'
+  get 'profile/create'
+  get 'profile/destroy'
+  get 'profile/edit'
+  get 'profile/update'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  devise_for :users
-  root to: 'pages#home'
+  # devise_for :users
+  # root to: 'pages#home'
 
-  resources :profiles
+  # resources :profiles
 
-  resources :onboardings
+  # resources :onboardings
 
-  resources :users
+  # resources :users
 
-  resources :checklists
+  # resources :checklists
 
-  resources :chatrooms, only: :show do
-    resources :messages, only: :create
-  end
-
+   # resources :chatrooms, only: :show do
+     #resources :messages, only: :create
+   # end
 end
