08888b55c4419425adc23113f5d2dacd15d6799b
RachSavoy <rachael.a.hakim@gmail.com>
2021-06-28 15:36:27 +0900

    FEAT: added route to user sign in in routes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,38 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery
 
-  def configure_permitted_parameters
-    # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  protected
 
-    # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
-  end
-
-  include Pundit
-
-  # Pundit: white-list approach.
-  after_action :verify_authorized, except: :index, unless: :skip_pundit?
-  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
-
-  # Uncomment when you *really understand* Pundit!
-  # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-  # def user_not_authorized
-  #   flash[:alert] = "You are not authorized to perform this action."
-  #   redirect_to(root_path)
-  # end
-  def default_url_options
-    { host: ENV["DOMAIN"] || "localhost:3000" }
+  def after_sign_in_path_for(resource)
+    stored_location_for(:user) || root_path
   end
 
   private
 
-  def skip_pundit?
-    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  def after_sign_out_path_for(resource)
+    stored_location_for(:user) || root_path
   end
-  # def set_time_zone
-  #   Time.zone = current_user.time_zone
-  # end
 
 end
