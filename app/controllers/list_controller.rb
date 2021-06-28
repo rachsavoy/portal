e70c0299eb7a9eb98ddbfeebb5339fa1419fef4d
RachSavoy <rachael.a.hakim@gmail.com>
2021-06-28 13:21:05 +0900

    FEAT: added show index and create to list controller.

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,16 +1,40 @@
 class ListController < ApplicationController
   def index
+    @list = policy_scope(List).order(created_at: :desc)
+    @list = current_user
+    @chatroom = current_user.chatroom
+    @message = Message.new
   end
 
   def show
   end
 
   def create
+    @list = Task.new(task_params)
+    @list.user = current_user
+    @list.task = @task
+
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render 'list/show'
+    end
+  end
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:content, :event_id)
+  end
+
+  def authorize_note
+    authorize @note
   end
 end
