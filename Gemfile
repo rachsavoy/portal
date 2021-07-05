d744a254762a85bf879a7f5c72d0663ee2ee7120
RachSavoy <rachael.a.hakim@gmail.com>
2021-07-05 09:55:01 +0900

    FEAT: added gem mail form for blog form page to be emailed upon completion.

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'pundit'
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass'
 gem 'simple_form'
+gem 'mail_form'
 
 group :development, :test do  gem 'pry-byebug'
   gem 'pry-rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,16 @@ class User < ApplicationRecord
   belongs_to :onboarding
   belongs_to :profile
   has_one_attached :photo
+
+  include MailForm::Delivery
+
+  append :remote_ip, :user_agent, :session
+  attributes :name, :email, :created_at
+
+  def headers
+    {
+      to: "your.email@your.domain.com",
+      subject: "User created an account"
+    }
+  end
 end
